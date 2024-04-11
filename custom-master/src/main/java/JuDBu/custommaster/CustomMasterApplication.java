260df90a7b4d780413a08848fd0f7441121ca242
ydh511 <ydh511@gmail.com>
2024-04-11 14:49:33 +0900

    first commit

---
+++ custom-master/src/main/java/JuDBu/custommaster/CustomMasterApplication.java
@@ -0,0 +1,13 @@
+package JuDBu.custommaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomMasterApplication.class, args);
+	}
+
+}
