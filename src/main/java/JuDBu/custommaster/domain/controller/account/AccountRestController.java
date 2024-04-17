060b71792c39e3dcb77fad6ca75618da33195418
ydh511 <ydh511@gmail.com>
2024-04-17 17:26:44 +0900

    Feat: mail인증
    Body : mail인증 api상으로 구현
    
    Tail : 완료

--- src/main/java/JuDBu/custommaster/domain/controller/account/AccountRestController.java
+++ src/main/java/JuDBu/custommaster/domain/controller/account/AccountRestController.java
@@ -1,15 +1,20 @@
 package JuDBu.custommaster.domain.controller.account;
 
+import JuDBu.custommaster.auth.facade.AuthenticationFacade;
+import JuDBu.custommaster.domain.entity.account.Account;
 import JuDBu.custommaster.domain.service.account.AccountService;
 import JuDBu.custommaster.domain.dto.account.AccountDto;
 import JuDBu.custommaster.domain.dto.account.CustomAccountDetails;
 import JuDBu.custommaster.domain.entity.account.Authority;
 import JuDBu.custommaster.auth.jwt.dto.JwtResponseDto;
+import JuDBu.custommaster.domain.service.account.MailService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @RestController
@@ -19,13 +24,15 @@ public class AccountRestController {
     private final UserDetailsManager manager;
     private final AccountService accountService;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationFacade authFacade;
+    private final MailService mailService;
 
-    @PostMapping("business-register")
+    @PostMapping("/business-register")
     public JwtResponseDto businessRegister(){
         return null;
     }
 
-    @PostMapping("register")
+    @PostMapping("/register")
     public String register(
             @RequestParam("username")
             String username,
@@ -50,7 +57,7 @@ public class AccountRestController {
         return "register done";
     }
 
-    @GetMapping("read/{id}")
+    @GetMapping("/read/{id}")
     public AccountDto readOne(
             @PathVariable("id")
             Long id
@@ -58,7 +65,7 @@ public class AccountRestController {
         return accountService.readOne(id);
     }
 
-    @PostMapping("update")
+    @PostMapping("/update")
     public String update(
             @RequestParam("username")
             String username,
@@ -79,11 +86,41 @@ public class AccountRestController {
         return "register done";
     }
 
-    @PostMapping("delete/{id}")
+    @PostMapping("/delete/{id}")
     public AccountDto deleteOne(
             @PathVariable("id")
             Long id
     ){
         return accountService.delete(id);
+    }
+
+    @PostMapping("/send-mail")
+    public String sendMail(){
+        Account account = authFacade.getAccount();
+        mailService.send(account.getUsername(), account.getEmail());
+        return "send mail";
+    }
+
+    @PostMapping("/check-mail")
+    public String checkCode(
+            @RequestParam
+            String authString
+    ){
+        Account account = authFacade.getAccount();
+
+        if (!mailService.checkTimeLimit5L(account.getUsername())){
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        }
+        if(!mailService.checkInfo(account.getUsername(), account.getEmail())){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        if(!mailService.checkMailAuth(account.getUsername())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        if (!mailService.checkCode(account.getUsername(), authString)){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        return "mailauth done";
     }
 }
