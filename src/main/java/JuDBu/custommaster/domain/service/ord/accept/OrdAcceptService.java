106189fba9be6f3d019541a6f75ce93b2dd3faea
jisoo0609 <jisu20000609@gmail.com>
2024-04-30 17:30:57 +0900

    Fix: 서비스 수정

--- src/main/java/JuDBu/custommaster/domain/service/ord/accept/OrdAcceptService.java
+++ src/main/java/JuDBu/custommaster/domain/service/ord/accept/OrdAcceptService.java
@@ -190,6 +190,6 @@ public class OrdAcceptService {
                 .orElseThrow(() ->new ResponseStatusException(HttpStatus.NOT_FOUND));
         log.info("shop Account: {}", shop.getAccount().getUsername());
 
-        return !shop.getAccount().equals(account);
+        return shop.getAccount().equals(account);
     }
 }
